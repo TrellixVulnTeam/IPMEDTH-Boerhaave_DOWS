d3f30a8cddfa285af0550b2659d9c8906c5f280d
s1115477 <s1115477@student.hsleiden.n>
2021-11-30 18:49:48 +0100

    lezen voorwerp optillen
    Er wordt nu gelezen welk voorwerp er wordt opgetild en in de array geplaats. Array is van 2D naar 1D gegaan, voor het logischer maken

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ import time
 Builder.load_file('info.kv');
 Builder.load_file('error.kv')
 
-voorwerpPlaatsen = [[1,0,1,1], [1,0,1,0]]
+voorwerpPlaatsen = [1,1,1,1,1,0,1,0]
 
 class RedCircle(Widget):
     pass
@@ -50,22 +50,20 @@ class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
         refresh_time = 0.5
-        #Clock.schedule_interval(self.timer, refresh_time)
+        Clock.schedule_interval(self.timer, refresh_time)
         self.cols = 1
 
 
         self.info = GreenCircle();
         self.add_widget(self.info)
-        self.checkErr()
+        #self.checkErr()
 
     def checkErr(self):
         count = 0
-        if sum(x.count(0) for x in voorwerpPlaatsen) > 1:
+        if voorwerpPlaatsen.count(0) > 1:
             self.clear_widgets();
             popupLayout = ErrorPopUp()
 
-            #backdrop = BackDrop();
-            #popupLayout.add_widget(backdrop)
             layout = GridLayout(cols= 4, size_hint=(0.6, 0.7))
             print(layout.size)
             with layout.canvas.before:
@@ -73,32 +71,41 @@ class MyGrid(GridLayout):
                 Rectangle(size=(layout.size))
                 print(layout.size)
             for rows in range(len(voorwerpPlaatsen)):
-                for col in range(len(voorwerpPlaatsen[rows])):
-                    if voorwerpPlaatsen[rows][col] == 0:
-                        #layout.add_widget(RedCircle())
-                        popupLayout.tafel.add_widget(RedCircle())
-                        print(popupLayout.tafel.size)
-                    else:
-                        popupLayout.tafel.add_widget(GreenCircle())
+                if voorwerpPlaatsen[rows] == 0:
+                    #layout.add_widget(RedCircle())
+                    popupLayout.tafel.add_widget(RedCircle())
+                    print(popupLayout.tafel.size)
+                else:
+                    popupLayout.tafel.add_widget(GreenCircle())
 
             #popupLayout.add_widget(layout)
             #popupLayout.add_widget(Label(text="dit is een test"))
             self.add_widget(popupLayout)
-        print(sum(x.count(0) for x in voorwerpPlaatsen))
-        #print(len(voorwerpPlaatsen))
 
     def timer(self, dt):
         if ser.in_waiting > 2:
-
             line = ser.readline().decode('utf-8').rstrip();
-            print(voorwerpPlaatsen[0])
-            print(line);
-            if "sn" in line:
-                self.clear_widgets()
-                self.add_widget(InfoSaturnMoon())
-            if "mn" in line:
-                self.clear_widgets()
-                self.add_widget(InfoMoon())
+            #print(line[0]);
+            self.arduinoCheck(line)
+
+
+
+    def arduinoCheck(self, message):
+        print(message[0])
+        if "None" in message:
+            print("test")
+            #print(message[0])
+            #print(voorwerpPlaatsen[0])
+            numberReader = int(message[0])
+            print(numberReader)
+            voorwerpPlaatsen[numberReader] = 0
+            print(voorwerpPlaatsen)
+        if "sn" in message:
+            self.clear_widgets()
+            self.add_widget(InfoSaturnMoon())
+        if "mn" in message:
+            self.clear_widgets()
+            self.add_widget(InfoMoon())
 
 
 
