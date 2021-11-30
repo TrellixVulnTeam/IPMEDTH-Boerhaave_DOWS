350ad5a72dda9adc7b24e3830d77ab02684f85fe
s1115477 <s1115477@student.hsleiden.n>
2021-11-30 14:49:44 +0100

    Error instructions
    eerst hand van de error instructies gemaakt. Begon gemaakt aan de popup. En een 2d array die checkt welke nfrreaders zijn opgepakt

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
@@ -10,6 +11,7 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.graphics import Color, Rectangle, Canvas
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ObjectProperty
 
 import serial
 import time
@@ -17,9 +19,24 @@ import time
 Builder.load_file('info.kv');
 Builder.load_file('error.kv')
 
-class ErrorPopUp(Widget):
+voorwerpPlaatsen = [[1,0,1,1], [1,0,1,0]]
+
+class RedCircle(Widget):
     pass
 
+class GreenCircle(Widget):
+    pass
+class BackDrop(Widget):
+    pass
+class ErrorPopUp(Widget):
+    tafel= ObjectProperty()
+    tafel_1 = ObjectProperty()
+    tafel_2 = ObjectProperty()
+    tafel_3 = ObjectProperty()
+    tafel_4 = ObjectProperty()
+
+
+    pass
 class InfoSaturnMoon(Widget):
     pass
 
@@ -33,14 +50,48 @@ class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
         refresh_time = 0.5
-        Clock.schedule_interval(self.timer, refresh_time)
+        #Clock.schedule_interval(self.timer, refresh_time)
         self.cols = 1
-        self.info = Instruction();
+
+
+        self.info = GreenCircle();
         self.add_widget(self.info)
+        self.checkErr()
+
+    def checkErr(self):
+        count = 0
+        if sum(x.count(0) for x in voorwerpPlaatsen) > 1:
+            self.clear_widgets();
+            popupLayout = ErrorPopUp()
+
+            #backdrop = BackDrop();
+            #popupLayout.add_widget(backdrop)
+            layout = GridLayout(cols= 4, size_hint=(0.6, 0.7))
+            print(layout.size)
+            with layout.canvas.before:
+                Color(0, 1,  0, 0.25)
+                Rectangle(size=(layout.size))
+                print(layout.size)
+            for rows in range(len(voorwerpPlaatsen)):
+                for col in range(len(voorwerpPlaatsen[rows])):
+                    if voorwerpPlaatsen[rows][col] == 0:
+                        #layout.add_widget(RedCircle())
+                        popupLayout.tafel.add_widget(RedCircle())
+                        print(popupLayout.tafel.size)
+                    else:
+                        popupLayout.tafel.add_widget(GreenCircle())
+
+            #popupLayout.add_widget(layout)
+            #popupLayout.add_widget(Label(text="dit is een test"))
+            self.add_widget(popupLayout)
+        print(sum(x.count(0) for x in voorwerpPlaatsen))
+        #print(len(voorwerpPlaatsen))
 
     def timer(self, dt):
-        if ser.in_waiting > 0:
+        if ser.in_waiting > 2:
+
             line = ser.readline().decode('utf-8').rstrip();
+            print(voorwerpPlaatsen[0])
             print(line);
             if "sn" in line:
                 self.clear_widgets()
