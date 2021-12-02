44d0bfeb62c3ac8ca97fa3a0607374231959a4c7
Manouk2000 <m.van.teeffelen@hotmail.com>
2021-12-02 15:13:05 +0100

    Basisscherm Layout af + in press_it random vraag functie gemaakt

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.core.image import Image
 from kivy.lang import Builder
+from kivy.config import Config
+import random
 
 Builder.load_file('BasisSchermLayout.kv')
+
 
 class MyLayout(Widget):
   def press_it(self):
@@ -18,13 +22,28 @@ class MyLayout(Widget):
     current += .20
     current_question += 1
 
+    questions = ['vraag1: testen', 'vraag2: Hallo', 'vraag3: hoi', 'vraag4: doeg']
+
+    while len(questions) > 0:
+      index = random.choice(questions)
+      self.ids.my_label_question.text = index
+      questions.remove(index)
+      print(questions)
+
     self.ids.my_progress_bar.value = current
     self.ids.my_label.value = current_question
     self.ids.my_label.text = f'Vraag {self.ids.my_label.value}'
+
+     
+
 
 class MyApp(App):
   def build(self):
     return MyLayout()
 
+
 if __name__ == '__main__':
+  Config.set('graphics', 'fullscreen', 'auto')
+  Config.set('graphics', 'window_state', 'maximized')
+  Config.write()
   MyApp().run()
