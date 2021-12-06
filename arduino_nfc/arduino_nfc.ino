9d247be7e55ac7c7acd7ab867b5cbb035aeac646
s1115477 <s1115477@student.hsleiden.n>
2021-12-06 15:15:05 +0100

    arduino reset knop
    als je de knop indrukt gaat de arduino eerst alles opnieuw lezen en verstuurd wat er momenteel opstaat.

--- arduino_nfc/arduino_nfc.ino
+++ arduino_nfc/arduino_nfc.ino
@@ -12,6 +12,8 @@ byte ssPins[] = {SS_PIN, SS_2_PIN, SS_ANSWER_PIN};
 MFRC522 mfrc522[NR_OF_READERS];
 bool statusReaders[NR_OF_READERS];
 
+int pushButton = 2; 
+bool buttonPressed = false;
 
 void setup() {
   Serial.begin(9600);
@@ -33,6 +35,7 @@ void setup() {
   }
 
   Serial.println("Tap RFID/NFC Tag on reader");
+  pinMode(pushButton, INPUT);
 }
 
 void check_code(String code, uint8_t numberReader){
@@ -60,39 +63,78 @@ void translate_input(byte * buffer, byte bufferSize, uint8_t numberReader ){
     check_code(NFCCode, numberReader);
 }
 
-void loop() {
-  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
-   // Serial.println(NR_OF_READERS);
-  
-    if(mfrc522[reader].PICC_IsNewCardPresent() ){
-      if(mfrc522[reader].PICC_ReadCardSerial()){
-       // !! DIT IS GEEN REDUCANT CODE. HIERDOOR WERKT HET, LAAT DEZE REGEL STAAN !!!! 
-       mfrc522[reader].PICC_ReadCardSerial();
-       if(statusReaders[reader] == false){
-         translate_input(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size, reader);
-         statusReaders[reader] = true;
-       
+void resetScan(){
+  for(uint8_t reader = 0; reader < NR_OF_READERS; reader++){
+    Serial.print("scanner momenteel: ");
+    Serial.println(statusReaders[reader]);
+     if(mfrc522[reader].PICC_IsNewCardPresent() ){
+        if(mfrc522[reader].PICC_ReadCardSerial()){
+          
+          mfrc522[reader].PICC_ReadCardSerial();
+          
+          translate_input(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size, reader);
           mfrc522[reader].PICC_HaltA(); // halt PICC
-          mfrc522[reader].PCD_StopCrypto1(); // stop encryption on PCD
-          }
-      }
-    }else{
-        if(statusReaders[reader] == true){
-          //checkt of het de laatste nfc reader (antwoord) en maakt bericht welk reader een voorwerp opgetild is
-          if(reader == (NR_OF_READERS - 1))
-          {
-            String arduinoMessage = "a_none";
-            Serial.println(arduinoMessage);
-            statusReaders[reader] = false;
-          }else{
-            String arduinoMessage = String(reader);
-            arduinoMessage = arduinoMessage + "_none";
-            Serial.println(arduinoMessage);
-            statusReaders[reader] = false;
-          }
+          mfrc522[reader].PCD_StopCrypto1();
         }
+     }
+     else{
+      String arduinoMessage = String(reader);
+      arduinoMessage = arduinoMessage + "_none";
+      Serial.println(arduinoMessage);
+     }
+     delay(100);
+  }
+}
+
+void loop() {
+  //checkt of de "reset" knop is in gedruk
+  if(digitalRead(pushButton)){
+      if(digitalRead(pushButton) && !(buttonPressed)){
+      buttonPressed = true;
+      Serial.println("reset");
+      resetScan();
+    }
+  }
+  //als de knop los gelaten wordt, return naar niet ingedrukte state
+  else if( !(digitalRead(pushButton)) && buttonPressed){
+      Serial.print("in");
+      buttonPressed = false;
+  }
+  //leest de veranderingen van de NFCreaders
+  else{
+    for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+     // Serial.println(NR_OF_READERS);
     
+      if(mfrc522[reader].PICC_IsNewCardPresent() ){
+        if(mfrc522[reader].PICC_ReadCardSerial()){
+         // !! DIT IS GEEN REDUCANT CODE. HIERDOOR WERKT HET, LAAT DEZE REGEL STAAN !!!! 
+         mfrc522[reader].PICC_ReadCardSerial();
+         if(statusReaders[reader] == false){
+           translate_input(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size, reader);
+           statusReaders[reader] = true;
+         
+            mfrc522[reader].PICC_HaltA(); // halt PICC
+            mfrc522[reader].PCD_StopCrypto1(); // stop encryption on PCD
+            }
+        }
+      }else{
+          if(statusReaders[reader] == true){
+            //checkt of het de laatste nfc reader (antwoord) en maakt bericht welk reader een voorwerp opgetild is
+            if(reader == (NR_OF_READERS - 1))
+            {
+              String arduinoMessage = "a_none";
+              Serial.println(arduinoMessage);
+              statusReaders[reader] = false;
+            }else{
+              String arduinoMessage = String(reader);
+              arduinoMessage = arduinoMessage + "_none";
+              Serial.println(arduinoMessage);
+              statusReaders[reader] = false;
+            }
+          }
       
+        
+      }
     }
   }
   
